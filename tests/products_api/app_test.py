68f4f58190c5599e1e5f74a47cea991f0b6cfe52
MJ Rossetti <datacreativellc@gmail.com>
2018-05-27 17:24:21 -0400

    Simplify by removing unused parts of the code

--- tests/products_api/app_test.py
+++ tests/products_api/app_test.py
@@ -3,14 +3,9 @@ import pytest # use pytest.set_trace() to drop an interactive breakpoint
 
 from products_api.app import app
 
-# adapted from https://serge-m.github.io/testing-json-responses-in-Flask-REST-apps-with-pytest.html
 @pytest.fixture
-def client(request):
-    test_client = app.test_client()
-    def teardown():
-        pass # databases and resourses have to be freed at the end. But so far we don't have anything
-    request.addfinalizer(teardown)
-    return test_client
+def client():
+    return app.test_client()
 
 #
 # HOME
