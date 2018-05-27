5fdce9a38881706dfe091fc4310b9d7829f19d58
MJ Rossetti <datacreativellc@gmail.com>
2018-05-26 20:37:20 -0400

    Moving configuration so it will be used on Heroku

--- products_api/__init__.py
+++ products_api/__init__.py
@@ -1,11 +1,4 @@
-from dotenv import load_dotenv
-import os
-
 from products_api.app import app
 import products_api.routes
-
-load_dotenv()
-
-app.secret_key = os.getenv("SECRET_KEY") or "my super secret"
 
 app.run
--- products_api/app.py
+++ products_api/app.py
@@ -1,3 +1,9 @@
+from dotenv import load_dotenv
 from flask import Flask
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+
+app.secret_key = os.getenv("SECRET_KEY") or "my super secret"
