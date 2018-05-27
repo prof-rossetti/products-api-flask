cc4633a14bb4c13eace969ae860d4406185054e4
MJ Rossetti <datacreativellc@gmail.com>
2018-05-27 16:55:33 -0400

    Cherry-pick these tests from other branch

--- tests/products_api/app_test.py
+++ tests/products_api/app_test.py
@@ -28,3 +28,24 @@ def test_hello(client):
 def test_hello_with_params(client):
     response = client.get('/hello?name=Jordan')
     assert "Hello, Jordan" in str(response.data)
+
+#
+# PRODUCTS
+#
+
+def test_list_products(client):
+    response = client.get('/products')
+    response_body = json.loads(response.data)
+    assert response.status_code == 200
+    assert isinstance(response_body, list)
+    assert len(response_body) == 20
+    assert isinstance(response_body[0], dict)
+
+def test_show_product(client):
+    response = client.get('/products/1')
+    response_body = json.loads(response.data)
+    attributes = list(response_body.keys())
+    assert response.status_code == 200
+    assert isinstance(response_body, dict)
+    assert attributes == ["aisle", "department", "id", "name", "price"]
+    assert int(response_body["id"]) == 1
