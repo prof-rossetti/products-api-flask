c7b8f3b98b21b3f21cb6de1acb262652da9e9699
MJ Rossetti <datacreativellc@gmail.com>
2018-06-01 17:27:38 -0400

    WIP - started exploring environment config --skip-ci

--- products_api/app.py
+++ products_api/app.py
@@ -7,3 +7,5 @@ load_dotenv()
 app = Flask(__name__)
 
 app.secret_key = os.getenv("SECRET_KEY") or "my super secret"
+
+#app.config['MY_ENV'] = os.getenv("MY_ENV") or "development"
---
+++ tests/app_test.py
@@ -0,0 +1,5 @@
+#import pytest
+#
+#def test_app(client):
+#    app_env = client.config["MY_ENV"]
+#    assert app_env == "test"
