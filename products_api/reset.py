0d2145f35845a73525d0ef59cee97ac8448c4b9f
MJ Rossetti <datacreativellc@gmail.com>
2018-06-02 20:48:55 -0400

    Bypass KeyError if env var not set

--- products_api/reset.py
+++ products_api/reset.py
@@ -5,7 +5,7 @@ from db import products_csv_filename, reset_products_file
 
 if __name__ == "__main__":
     load_dotenv()
-    app_env = os.environ["FLASK_ENV"] or "development"
+    app_env = os.environ.get("FLASK_ENV") or "development"
     csv_filename = products_csv_filename(app_env)
     print(f"RESETTING PRODUCTS FILE: {csv_filename}")
     reset_products_file(filename=csv_filename)
